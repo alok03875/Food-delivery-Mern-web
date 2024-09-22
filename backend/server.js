cd9a61440943fd4df1bfbce4dc3757b8c5bea76c
Alok kushwaha <154000371+alok03875@users.noreply.github.com>
2024-09-23 03:04:41 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config"
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+
+// middleware
+app.use(express.json())         // to parse req from fruntend to backend
+app.use(cors())                 // to access backend from any fruntend
+
+// db connection
+connectDB();
+
+// API endPoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
+
+
+app.get("/", (req, res) => {
+    res.send("API working");
+})
+
+app.listen(port, () => {
+    console.log(`Server are running on http://localhost:${port}/`)
+})
