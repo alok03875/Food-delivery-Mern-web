6d6447e2cd350eaed806164812b4d47eb38ce1e7
Alok kushwaha <154000371+alok03875@users.noreply.github.com>
2024-09-22 14:39:35 -0800

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,10 +7,9 @@ import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { url } from './assets/assets'
 
 const App = () => {
-
-  const url = " http://localhost:4000";
 
   return (
     <div>
