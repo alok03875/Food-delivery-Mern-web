25a285a1fa37fc415af5ffe956ba6bce32d84193
Alok kushwaha <154000371+alok03875@users.noreply.github.com>
2025-01-16 02:23:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,32 @@ import userRouter from "./routes/userRoute.js";
 import "dotenv/config"
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import path from 'path'
+import { fileURLToPath } from "url";
 
-// app config
 const app = express()
 const port = process.env.PORT || 4000;
 
-
-// middleware
 app.use(express.json())         // to parse req from fruntend to backend
 app.use(cors())                 // to access backend from any fruntend
 
-// db connection
 connectDB();
 
-// API endPoints
+const _dirname = path.resolve();
+
+// Get the current file path and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/api/food", foodRouter)
-app.use("/images", express.static("uploads"))
-app.use("/api/user", userRouter)
+app.use("/images", express.static(path.join(__dirname, "uploads")));  
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 app.get("/", (req, res) => {
     res.send("API working");
